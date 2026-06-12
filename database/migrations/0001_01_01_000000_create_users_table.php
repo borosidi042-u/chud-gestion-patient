45d66e6432b10ed051e615951962f907caf6547f
borosidi042-u <borosidi042@gmail.com>
2026-06-12 20:13:37 +0100

    Update 0001_01_01_000000_create_users_table.php

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -15,8 +15,8 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->string('role')->default('user');
-            $table->boolean('approved')->default(false);
+            $table->string('role')->default('admin');
+            $table->boolean('approved')->default(true);
             $table->rememberToken();
             $table->timestamps();
         });
