e4dc47028ab9aa9914be4ff29732be08cb6ef77e
borosidi042-u <borosidi042@gmail.com>
2026-06-13 08:40:01 +0100

    Update password for database seeder user

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,7 @@ public function run(): void
     \App\Models\User::create([
         'name' => 'Administrateur CHUD',
         'email' => 'borosidi042@gmail.com', // L'email pour te connecter
-        'password' => bcrypt('2t8NjPR9gvxQ5RV^) '),   // Ton mot de passe secret
+        'password' => bcrypt(administrateur '),   // Ton mot de passe secret
         'role' => 'admin',    // Ajuste si ta colonne s'appelle autrement
         
     ]);
